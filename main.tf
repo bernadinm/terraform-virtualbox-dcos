cacac9ff7909e24595b7ae37dc9c4a3adb90bdba
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-10-24 15:52:17 -0700

    deploys coreos but fails startup check

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
 resource "virtualbox_vm" "node" {
     count = 2
-    name = "${format("node-%02d", count.index+1)}"
-    url = "https://vagrantcloud.com/ubuntu/boxes/trusty64/versions/20180206.0.0/providers/virtualbox.box"
-    image = "./virtualbox-ubuntu.box"
+    name = "${format("node-%02d", count.index)}"
+#   url = "https://vagrantcloud.com/ubuntu/boxes/trusty64/versions/20180206.0.0/providers/virtualbox.box"
+#   url = "https://app.vagrantup.com/AntonioMeireles/boxes/coreos-stable/versions/835.12.0/providers/virtualbox.box"
+    url = "https://app.vagrantup.com/amam/boxes/coreos-stable/versions/1855.4.0.1/providers/virtualbox.box"
+#   image = "./virtualbox-ubuntu.box"
+    image = "./virtualbox-coreos.box"
     cpus = 2
     memory = "512 mib",
-    user_data = "${file("user_data")}"
+#    user_data = "${file("user_data")}"
 
 
     network_adapter {
